2e97ee081752f0c78bc172b6c220454d0d0317c5
Justin Fung <83911295+Deepdive543443@users.noreply.github.com>
2023-08-06 21:18:30 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,51 @@
-// Opencv lib
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-
-//NCNN lib
+#include <vector>
+#include <opencv2/opencv.hpp>
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/highgui/highgui.hpp>
 #include "net.h"
-
-// Standard lib
-#include <iostream>
-#include <time.h>
-//common
 #include "utils.h"
 
 
 int main() 
 {
+
+    int num_img = 0;
     srand(time(NULL));
     // Load network
     ncnn::Net progan;
-    progan.load_param("model/script_pro.ncnn.param");
-    progan.load_model("model/script_pro.ncnn.bin");
+    progan.load_param("../../model/ProGressive.ncnn.param");
+    progan.load_model("../../model/ProGressive.ncnn.bin");
     printf("Loaded");
 
     // Generate random noise
-    ncnn::Mat noise = randn_mat(32, 16, 1, rand());
-    noise = noise.reshape(1, 1, 512);
-    shape(noise);
-  
-    // Forward
-    ncnn::Extractor ex = progan.create_extractor();
-    ex.input("in0", noise);
-    ncnn::Mat outputs;
-    ex.extract("out0", outputs);
-    printf("Output's shape");
-    shape(outputs);
-    post_process_img(outputs);
+    while(true){
+        ncnn::Mat noise = randn_mat(512, 1, 1, rand());
+        noise = noise.reshape(1, 1, 512);
+        shape(noise);
 
-    // Display the generated images
-    cv::Mat cv_img = cv::Mat::zeros(outputs.w, outputs.h, CV_8UC3);
-    outputs.to_pixels(cv_img.data, ncnn::Mat::PIXEL_BGR2RGB);
-    shape(cv_img);
-    cv::imshow("Testing", cv_img);
-    cv::waitKey(0);
+    
+        // Forward
+        ncnn::Extractor ex = progan.create_extractor();
+        ex.input("in0", noise);
+        ncnn::Mat outputs;
+        ex.extract("out0", outputs);
+        printf("Output's shape");
+        shape(outputs);
+        post_process_img(outputs);
+
+        // Display the generated images
+        cv::Mat cv_img = cv::Mat::zeros(outputs.w, outputs.h, CV_8UC3);
+        outputs.to_pixels(cv_img.data, ncnn::Mat::PIXEL_BGR2RGB);
+        shape(cv_img);
+        cv::imshow("Testing", cv_img);
+        cv::waitKey(0);
+
+        num_img++;
+        
+        if(num_img > 10){
+            break;
+        }
+    }
+    
     return 0;
 }
