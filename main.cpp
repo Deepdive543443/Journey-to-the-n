6fbae4477a730d2b8567f5eb337f4074d39f403d
Justin Fung <83911295+Deepdive543443@users.noreply.github.com>
2023-08-30 22:16:32 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,16 +8,15 @@
 
 int main() 
 {
-
-    int num_img = 0;
     srand(time(NULL));
     // Load network
     ncnn::Net progan;
-    progan.load_param("../../model/ProGressive.ncnn.param");
-    progan.load_model("../../model/ProGressive.ncnn.bin");
+    progan.load_param("../../models/Progressive.ncnn.param");
+    progan.load_model("../../models/Progressive.ncnn.bin");
     printf("Loaded");
 
     // Generate random noise
+    int key;
     while(true){
         ncnn::Mat noise = randn_mat(512, 1, 1, rand());
         noise = noise.reshape(1, 1, 512);
@@ -33,18 +32,23 @@ int main()
         shape(outputs);
         post_process_img(outputs);
 
+
         // Display the generated images
         cv::Mat cv_img = cv::Mat::zeros(outputs.w, outputs.h, CV_8UC3);
         outputs.to_pixels(cv_img.data, ncnn::Mat::PIXEL_BGR2RGB);
         shape(cv_img);
         cv::imshow("Testing", cv_img);
         cv::waitKey(0);
-
-        num_img++;
+        key = cv::waitKey(33);
         
-        if(num_img > 10){
+
+        if (key == 27)
+        {
             break;
-        }
+        }else if(key == -1)
+        {
+            continue;
+        };
     }
     
     return 0;
--- utils.h
+++ utils.h
@@ -1,7 +1,8 @@
 // Opencv lib
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+// #include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 
 //NCNN lib
 #include "net.h"
